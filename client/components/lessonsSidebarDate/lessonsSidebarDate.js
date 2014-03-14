239714c41f991cef10a77b3c7ec9bb4d9c7bdd89
duma <dumaron@gmail.com>
2014-03-14 21:22:36 +0100

    filtraggio nelle sidebar per yogi e lezioni

--- client/components/lessonsSidebarDate/lessonsSidebarDate.js
+++ client/components/lessonsSidebarDate/lessonsSidebarDate.js
@@ -5,3 +5,20 @@ Template.lessonsSidebarDate.lesson = function() {
 Template.lessonsSidebarDate.humanForm = function(date) {
 	return moment(date,'YYYY-MM-DD').format('DD/MM/YYYY');
 };
+
+Template.lessonsSidebarDate.events({
+	'keyup input': function(event) {
+		var text = event.srcElement.value.toLocaleLowerCase();
+		var voices = $('#lessonsDateSidebar').find('li');
+		if (text == '')
+			voices.css('display','show');
+		else {
+			voices.filter(function(idx, elem){
+				return $(elem).find('a').html().toLowerCase().indexOf(text) == -1;
+			}).css('display', 'none');
+			voices.filter(function(idx, elem){
+				return $(elem).find('a').html().toLowerCase().indexOf(text) != -1;
+			}).css('display', 'show');
+		}
+	}
+});
--- client/components/yogisSidebar/yogisSidebar.js
+++ client/components/yogisSidebar/yogisSidebar.js
@@ -1,3 +1,20 @@
 Template.yogisSidebar.yogi = function() {
 	return Yogis.find({}, {sort: {name:1}});
 };
+
+Template.yogisSidebar.events({
+	'keyup input': function(event) {
+		var text = event.srcElement.value.toLocaleLowerCase();
+		var voices = $('#yogisSidebar').find('li');
+		if (text == '')
+			voices.css('display','show');
+		else {
+			voices.filter(function(idx, elem){
+				return $(elem).find('a').html().toLowerCase().indexOf(text) == -1;
+			}).css('display', 'none');
+			voices.filter(function(idx, elem){
+				return $(elem).find('a').html().toLowerCase().indexOf(text) != -1;
+			}).css('display', 'show');
+		}
+	}
+});
