69d783043c9f163fde56973f31112ab3b1654e07
Duma <dumaron@gmail.com>
2014-06-15 18:56:19 +0200

    Rimozione di un iscritto da una lezione

--- client/pages/lessonByDate/lessonByDate.js
+++ client/pages/lessonByDate/lessonByDate.js
@@ -5,3 +5,19 @@ Template.lessonByDate.findYogiName = function(data) {
 Template.lessonByDate.humanForm = function(date) {
 	return moment(date,'YYYY-MM-DD').format('DD/MM/YYYY');
 };
+
+
+Template.lessonByDate.events({
+	'click .remove': function(evt) {
+		var answer = confirm('Vuoi veramente rimuovere questo utente dalla lezione?');
+		if (answer) {
+			var yogiId = evt.target.parentNode.parentNode.id.replace('yogi-','');
+			var yogi = Yogis.findOne(yogiId);
+			var lessonId = Router.current().params.id;
+			Lessons.update(lessonId, {$pull: {presents:yogiId}});
+			if (yogi.subType == 'single') {
+				Yogis.update(yogiId, {$inc: {lessons:1}});
+			}
+		}
+	}
+});
